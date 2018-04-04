b97ae18d734e0d94dd6533db06aa79aa04f112f1
ryanstapleton <megalixir.1@gmail.com>
2018-04-04 13:34:06 -0600

    Added ternary operators lesson

---
+++ ternary-operators.js
@@ -0,0 +1,7 @@
+const page = 'About';
+
+                    //   conditional  ?  TRUE Block    ELSE  FALSE block
+console.log(`class=${ page === 'Home' ? 'master-layout' : 'secondary-layout' }`)
+
+// == -> does not compare type, only value
+// === -> compares type and value
