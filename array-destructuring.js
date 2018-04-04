932c0a4969b8b9b80e17d417b89319a8db40f9c3
ryanstapleton <megalixir.1@gmail.com>
2018-04-04 14:09:17 -0600

    Array deconstruction lesson

---
+++ array-destructuring.js
@@ -0,0 +1,11 @@
+const apiList = [
+  'https://api.dailysmarty.com/posts',
+  'https://api.github.com/users/jordanhudgens/repos',
+  'https://api.github.com/users/jordanhudgens'
+]
+
+const [posts, repos, profile] = apiList;
+
+console.log(posts);
+console.log(repos);
+console.log(profile);
