700141f437ccc496a14702b8a29e6f5c9bdc0a55
ryanstapleton <megalixir.1@gmail.com>
2018-04-04 13:22:02 -0600

    String-interpolation lesson

---
+++ const-var.js
@@ -0,0 +1,22 @@
+// var city = 'Scottsdale';
+// console.log(city);
+
+// let city = 'Scottsdale';
+// console.log(city);
+
+// const city = 'Scottsdale';
+// console.log(city);
+
+
+
+// let city = 'Scottsdale';
+// console.log(city);
+
+// city = 'Pheonix'
+
+
+
+const city = 'Scottsdale';
+console.log(city);
+
+city = 'Pheonix'
